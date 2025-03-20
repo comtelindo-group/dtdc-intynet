6a4002a101ff93bc4af423ed57c66bd9504ac1b6
Hengki2003 <hengkiagng@gmail.com>
2025-03-20 13:25:53 +0800

    add

--- app/Constant.php
+++ app/Constant.php
@@ -9,6 +9,7 @@ class Constant
         "Tertarik dengan produk Intynet Smart 20 Mbps" => "Tertarik dengan produk Intynet Smart 20 Mbps",
         "Tertarik dengan produk Intynet Family 30 Mbps" => "Tertarik dengan produk Intynet Family 30 Mbps",
         "Tertarik dengan produk Intynet Maxima 50 Mbps" => "Tertarik dengan produk Intynet Maxima 50 Mbps",
+        "Tertarik dengan produk Intynet 100 Mbps" => "Tertarik dengan produk Intynet 100 Mbps",
         "Hanya taruh brosur" => "Hanya taruh brosur",
         "Tidak tertarik" => "Tidak tertarik",
     ];
